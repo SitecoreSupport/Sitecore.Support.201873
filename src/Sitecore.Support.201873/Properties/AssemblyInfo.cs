79901e7615ef2c4f8a46a2abe16ff80d168464a5
Ivan Sheyenko <ivs@sitecore.net>
2018-01-12 16:22:02 +0200

    original code

---
+++ src/Sitecore.Support.201873/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.201873")]
+[assembly: AssemblyProduct("Sitecore.Support.201873")]
+[assembly: ComVisible(false)]
